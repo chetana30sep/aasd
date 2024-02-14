3594cec7cf94c82feaa5d78838ced29a54e3986e
chetana11march <chetana.sadare11march@gmail.com>
2024-02-14 23:43:04 +0530

    commit

---
+++ Telecom-Project-1/src/test/java/com/example/TelecomProject1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+//adding first dev
+		//adding code for second developer
+//third
+		//four
+	}
+
+}
